8ba18c21445ffd5a55d714f86eb75417f2260c50
Тазеев Алмаз <tyj5resd@gmail.com>
2024-03-08 20:02:13 +0300

    wtf login

--- UserService.Api/obj/Debug/net8.0/UserService.Api.AssemblyInfo.cs
+++ UserService.Api/obj/Debug/net8.0/UserService.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserService.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+496bea489335fdaa2d04a944af2657b1e86a5bf0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af477796f2bfe72a3771fe6eb83f0851f8f46f95")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserService.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserService.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
